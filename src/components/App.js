6b87f73eed22b1056e31ab37e331c4ada0b324cf
finley <finley.lu@essist.com.tw>
2016-08-19 09:53:14 +0800

    Install font-awesome and integrated into project.
    $ npm install --save font-awesome font-awesome-webpack less
    
    	modified:   cfg/defaults.js
    	modified:   package.json
    	modified:   src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+require('font-awesome/scss/font-awesome.scss');
+
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyAwesomeReactComponent from './MyAwesomeReactComponent';
@@ -13,6 +15,12 @@ let App = () => (
         <div class="test-classname-transfer">
             <MyAwesomeReactComponent />
             <DialogExampleDialogDatePickerComponent />
+            <ul>
+                <li><a href="https://www.facebook.com/ethangroupofficial/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+                <li><a href="https://twitter.com/ethangroup" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+                <li><a href="https://plus.google.com/100310107127062073459/about" target="_blank"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+                <li><a href="https://www.linkedin.com/company/ethan-group" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+            </ul>
         </div>
     </MuiThemeProvider>
 );
